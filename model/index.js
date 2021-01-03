6503ccdfc3fbdcd08937ac5f4f302060b11fbfc2
Lrrr Omicron <lrrr@null.net>
2021-01-02 21:18:47 -0500

    Saving and deleting of saves works.  Now for loading

--- model/index.js
+++ model/index.js
@@ -9,7 +9,23 @@
 // we can persist the whole thing as json if we want but for now I'm just using // localStorage.
 
 let serialize = require('serialize-javascript');
-const deserialize = (str) => {return eval(str)};
+const deserialize = (str) => {
+  if (str === undefined) { return undefined; }
+  if (str == null) { return null; }
+  return eval(str);
+};
+
+let lastSaveIndexStr = localStorage.getItem("lastSaveIndex");
+let lastSaveIndex = 0;
+if (lastSaveIndexStr) {
+  lastSaveIndex = deserialize(lastSaveIndexStr); 
+  if (!lastSaveIndex) { 
+    lastSaveIndex = 0;
+  }
+  localStorage.lastSaveIndex = serialize(lastSaveIndex);
+}
+
+
 
 const getSavesData = () => {
   if (localStorage.getItem("saves") == null ) {
@@ -20,15 +36,23 @@ const getSavesData = () => {
 
 const initSavesData = (howMany) => {
   let saves = new Array(howMany);
+  for ( let i = 0; i < howMany; i++) {
+    saves[i] = initSaveData(i, null);
+  }
+  console.log("Saves:");
+  console.log(saves);
   localStorage.setItem("saves", serialize(saves) );
 }
 
-let db = {
-  properties: {
-    saveIndex: null,
-    saveDate:  new Date()
-  }
+const initSaveData = (saveIndex, saveDate) => {
+  return {
+    "saveIndex": saveIndex,
+    "saveDate": saveDate 
+  };
 };
+
+
+let db = initSaveData(lastSaveIndex,new Date());
 
 const init = () => {
   console.log("--> model.init()");
@@ -43,8 +67,7 @@ const init = () => {
 const load = (which) => {
   console.log("--> model.load(" + which + ")");
   if (which == null) {
-    console.log("Null passed for save slot to load");
-    init();
+    throw new RangeError("Null passed for save slot to load");
   }
 
   if (!localStorage.saves) {
@@ -57,10 +80,8 @@ const load = (which) => {
     db = saves[which];
   } else {
     let err = "Error invalid save index - did not exist: " + which;
-    console.log(err);
-    alert( "No save in slot: " + which + " starting new game." );
-    init();
-   }
+    throw new RangeError(err);
+  }
   console.log("<-- model.load(" + which + ")");
 };
 
@@ -77,14 +98,43 @@ const save = (which) => {
   }
   let saves = deserialize(localStorage.saves);
   console.log("Saving db into slot: " + which );
-  saves[which] = db;
+
+  db.saveIndex = which; 
+  db.saveDate = new Date(); 
+  saves[which] = db; 
+  lastSaveIndex = which;
+
   console.log("Slot " + which + "saved.");
   localStorage.saves = serialize(saves);
+  localStorage.lastSavedIndex = serialize(lastSaveIndex);
   console.log("<-- model.save(" + which + ")");
 };
+
+const deleteSave = (which) => {
+  console.log("--> model.deleteSave(" + which + ")");
+  if (which == null) { throw new RangeError("Null passed for save slot to delete"); }
+
+  if (!localStorage.saves) {
+    console.log("No existing saves");
+    init();
+  }
+  let saves = deserialize(localStorage.saves);
+  console.log("Deleting save db from slot: " + which );
+
+  saves[which] = null; 
+  if ( lastSaveIndex == which ) {
+    lastSaveIndex = null;
+  } 
+  console.log("Deleted slot " + which + ".");
+  localStorage.saves = serialize(saves);
+  localStorage.lastSavedIndex = serialize(lastSaveIndex);
+  console.log("<-- model.deleteSave(" + which + ")");
+};
+
 
 exports.getSavesData = getSavesData;
 exports.init = init;
 exports.load = load;
 exports.save = save;
+exports.deleteSave = deleteSave;
 exports.db = db; // the currently loaded game
--- stage/index.js
+++ stage/index.js
@@ -8,22 +8,17 @@ import model from "../model";
 
 const onBodyLoad = () => {
   console.log("--> stage.onBodyLoad()");
-  game.load();
-  showUiComponent("header-component", "header");
-  showUiComponent("passage-component", "welcome");
-  showUiComponent("footer-component", "footer");
+  try {
+    game.load();
+    showUiComponent("header-component", "header");
+    showUiComponent("passage-component", "welcome");
+    showUiComponent("footer-component", "footer");
+  } catch (err) {
+    console.log("Caught exception:");
+    console.log(err);
+    alert("Caught exception - see console.");
+  }  
   console.log("<-- stage.onBodyLoad()");
-};
-
-
-const fatalError = (message, code) =>  {
-    let msg = "FATAL ERROR";
-    if ( ! ( (code == undefined) || (code == null) ) ) {
-      msg += " CODE " + code;
-    }
-    msg += ": <<" + message + ">>";
-    console.log(msg);
-    alert(msg);
 };
 
 
@@ -39,6 +34,7 @@ const openSaveModal = () => {
   console.log("Saves data:")
   console.log(savesData);
   for (let which = 0; which < savesData.length; which++ ) {
+    let saveData = savesData[which]; // saveIndex == which
     let dispWhich = which + 1;
     let p = document.createElement("p");
     let saveButton = document.createElement("button");
@@ -46,31 +42,91 @@ const openSaveModal = () => {
     saveButton.innerHTML="Save " + dispWhich;
     saveButton.onclick=function () { stage.doSave(which); };
     p.appendChild(saveButton);
+    let saveDateText = getSaveDateText(saveData);
+    let deleteSaveButton = document.createElement("button");
+    deleteSaveButton.id="delete-save-" + which;
+    deleteSaveButton.innerHTML="Delete save: " + dispWhich + " made at: " + saveDateText;
+    deleteSaveButton.onclick=function () { stage.deleteSave(saveData.saveIndex); };
+    if ( saveData && saveData.saveDate ) {
+      deleteSaveButton.disabled = false;
+    } else  {
+      deleteSaveButton.disabled = true;
+    }
+    p.appendChild(deleteSaveButton);
+
     saves.appendChild(p);
     saveElt.style.display = "block";
   }
   console.log("<-- stage.openSaveModal()" );
 };
 
+const getSaveDateText = (saveData) => {
+  let saveDateText = "--/--/-- --:--:--";
+  let date = null;
+  if (saveData) { date = saveData.saveDate; }
+
+  if (date) {
+    saveDateText = ("0" + (date.getFullYear() - 2000)).slice(-2) + '/' + 
+      ("0" +  (date.getMonth() + 1 )).slice(-2) + '/' + 
+      ("0" + (date.getDate() + 1)).slice(-2) + ' ' + 
+      ("0" + date.getHours()).slice(-2) + ":" + 
+      ("0" + date.getMinutes()).slice(-2) + ":" + 
+      ("0" + date.getSeconds()).slice(-2);
+  }
+  return saveDateText;
+};
+
 const doSave = (which) => {
   console.log("--> stage.doSave(" + which +")" );
   model.save(which);
+  // no need to update display to reflect change 
+  // since next step is to close the dialog
+  alert("Saved in slot: " + ( which + 1) );
   closeSaveModal();
   console.log("<-- stage.doSave("+ which + ")" );
+};
+
+const deleteSave = (which) => {
+  console.log("--> stage.deleteSave(" + which +")" );
+ 
+  let savesData = model.getSavesData();
+  if (! savesData ) { 
+    throw new Error("no saves data");
+  }
+  let saveData = savesData[which];
+  if (! saveData ) {
+    throw new Error("no save data for index: " + which );
+  }     
+
+  if(confirm("Are you sure you want to delete save: " + ( which + 1 + "?") ) ) {
+    let deleteSaveButtonId = "delete-save-" + which;
+    let deleteSaveButton = document.getElementById(deleteSaveButtonId);
+    if (! deleteSaveButton) {
+      throw new Error("no such delete save button in document: " + deleteSaveButtonId );
+    }
+
+    deleteSaveButton.disabled = true;
+    let dispWhich = which + 1;
+    deleteSaveButton.innerHTML="Delete save: " + dispWhich + " made at: " + getSaveDateText(null);
+
+    model.deleteSave(which);
+  }
+ 
+  console.log("<-- stage.deleteSave("+ which + ")" );
 };
 
 const cancelSaveModal = () => {
   console.log("--> stage.cancelSaveModal()" );
   closeSaveModal();
   console.log("<-- stage.cancelSaveModal()" );
-}
+};
 
 const closeSaveModal = () => {
   console.log("--> stage.cancelSaveModal()" );
   let saveElt = document.getElementById(saveModalComponentId);
   saveElt.style.display = 'none'; // close the dialog
   console.log("<-- stage.cancelSaveModal()" );
-}
+};
 
 
 const showUiComponent = (where, which) => {
@@ -78,20 +134,20 @@ const showUiComponent = (where, which) => {
 
   if (where == which) {
     let msg = "where must not equal which: " + where + ", " + which;
-    return fatalError(msg);
+    throw new Error(msg);
   }
 
   let whichElt = document.getElementById(which);
 
   if (! whichElt ) {
     let msg = "NO SUCH PASSAGE MATCHING: " + which;
-    return fatalError(msg);
+    throw new Error(msg);
   }
 
   let whereElt = document.getElementById(where);
   if (! whereElt ) {
     let msg = "NO SUCH DOCUMENT ENTITY MATCHING: " + where;
-    return fatalError(msg);
+    throw new Error(msg);
   }
 
   whereElt.innerHTML = whichElt.innerHTML;
@@ -104,4 +160,5 @@ exports.onBodyLoad = onBodyLoad;
 exports.showPassage = showUiComponent;
 exports.openSaveModal = openSaveModal;
 exports.doSave = doSave;
+exports.deleteSave = deleteSave;
 exports.cancelSaveModal = cancelSaveModal;
